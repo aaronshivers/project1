bec78028a35c4a32c210686c5dc38b92aeb36e5a
aaronshivers <aaron@moonmail.org>
2019-06-22 22:43:47 -0500

    setup route for /users

--- application.py
+++ application.py
@@ -1,14 +1,16 @@
 import os
 
 from dotenv import load_dotenv
-from flask import Flask, session, jsonify
+from flask import Flask, session, jsonify, request
 from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from flask_marshmallow import Marshmallow
 import requests
 
 app = Flask(__name__)
-load_dotenv()
+# load_dotenv()
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -20,9 +22,32 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+# Initialize Marshmallow
+ma = Marshmallow(app)
+
+# User Model
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(100), nullable=False)
+  password = db.Column(db.String(100), nullable=False)
+
+  def __init__(self, name, password):
+    self.name = name
+    self.password = password
+
+# User Schema
+class UserSchema(ma.Schema):
+  class Meta:
+    # Fields to Expose
+    fields = ('id', 'name', 'password')
+
+# Initialize User Schema
+user_schema = UserSchema(strict=True)
+users_schema = UserSchema(many=True, strict=True)
 
 @app.route("/")
 def index():
@@ -30,3 +55,24 @@ def index():
   res = requests.get(url, params={"key": os.getenv('GOODREADS_KEY'), "isbns": "9781632168146"})
   json = res.json()
   return jsonify(json)
+
+@app.route('/users', methods=['POST', 'GET'])
+def users():
+  if request.method == 'POST':
+
+    name = request.json['name']
+    password = request.json['password']
+    new_user = User(name, password)
+
+    try:
+      db.session.add(new_user)
+      db.session.commit()
+      return user_schema.jsonify(new_user)
+    except Exception as e:
+      print(e)
+      return 'The user could not be added.'
+
+  else:
+    all_users = User.query.all()
+    result = users_schema.dump(all_users)
+    return jsonify(result.data)
